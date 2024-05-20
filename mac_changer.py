c34c63f36fe900a0dd659fd1bdbbcb6e2b9512ec
awaisn005 <awaisnaseer005@gmail.com>
2024-05-19 22:47:15 -0400

    Create mac_changer.py

---
+++ mac_changer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change mac")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please specify an interface")
+    elif not options.new_mac:
+        parser.error("Please specify a new mac address")
+    return options
+
+
+def change_mac(interface, new_mac):
+    print("Changing MAC Address for interface" + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
